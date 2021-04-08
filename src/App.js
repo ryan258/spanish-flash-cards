cb1dc46b150cd714338ec249ede7c1a5057577fc
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-08 13:46:44 -0500

    Add a header with subject selection and question count.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 
 import './app.css'
 import FlashCardList from './components/FlashCardList'
 
 function App() {
-  const [flashcards, setFlashCards] = useState(SAMPLE_FLASHCARDS)
+  /* const [flashcards, setFlashCards] = useState(SAMPLE_FLASHCARDS) */
+  const [flashcards, setFlashCards] = useState([])
+  const [categories, setCategories] = useState([])
 
-  // call in the questions from the API
+  const categoryEl = useRef()
+  const amountEl = useRef()
+
+  // generate all the different categories
   useEffect(() => {
-    axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
-      setFlashCards(
-        res.data.results.map((questionItem, index) => {
-          const answer = decodeString(questionItem.correct_answer)
-          const options = [
-            ...questionItem.incorrect_answers.map((incorrectAnswer) => decodeString(incorrectAnswer)),
-            // questionItem.correct_answer
-            answer
-          ]
-          return {
-            id: `${index}-${Date.now()}`,
-            question: decodeString(questionItem.question),
-            answer: answer,
-            options: options.sort(() => Math.random() - 0.5)
-          }
-        })
-      )
-      console.log(res.data)
+    axios.get('https://opentdb.com/api_category.php').then((res) => {
+      // console.log(res.data)
+      setCategories(res.data.trivia_categories)
     })
   }, [])
+
+  // call in the questions from the API
+  useEffect(() => {}, [])
 
   function decodeString(str) {
     const textArea = document.createElement('textarea')
@@ -36,11 +29,62 @@ function App() {
     return textArea.value
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios
+      .get('https://opentdb.com/api.php', {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value
+        }
+      })
+      .then((res) => {
+        setFlashCards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer)
+            const options = [
+              ...questionItem.incorrect_answers.map((incorrectAnswer) => decodeString(incorrectAnswer)),
+              // questionItem.correct_answer
+              answer
+            ]
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - 0.5)
+            }
+          })
+        )
+        console.log(res.data)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <p>beep</p>
-      </header>
+      </header> */}
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map((category) => {
+              return (
+                <option value={category.id} key={category.id}>
+                  {category.name}
+                </option>
+              )
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
       <div className="container">
         <FlashCardList flashcards={flashcards} />
       </div>
@@ -50,6 +94,7 @@ function App() {
 
 export default App
 
+/*
 const SAMPLE_FLASHCARDS = [
   {
     id: 1,
@@ -70,3 +115,4 @@ const SAMPLE_FLASHCARDS = [
     options: ['Beep', 'Boop', 'PIZZA', 'Marklar']
   }
 ]
+*/
